6861024430f87f7416edcaa2dead3b8e4fbb09e9
JL-er <997529190@qq.com>
2024-06-25 14:53:04 +0800

    1

--- gen.py
+++ gen.py
@@ -1,6 +1,5 @@
 import os, sys, torch, time
 import numpy as np
-from transformers import AutoModelForCausalLM, AutoTokenizer
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 current_path = os.path.dirname(os.path.abspath(__file__))
 print(current_path)
@@ -9,12 +8,10 @@ print(current_path)
 os.environ['RWKV_JIT_ON'] = '1'
 os.environ["RWKV_CUDA_ON"] = '1' # '1' to compile CUDA kernel (10x faster), requires c++ compiler & cuda libraries
 os.environ["RWKV_fla_ON"] = '0' # use fla
-CHAT_LANG = 'Chinese'
 from rwkv.model import RWKV # pip install rwkv
 from rwkv.utils import PIPELINE, PIPELINE_ARGS
 
 model = RWKV(model='/home/rwkv/JL/model/RWKV-x060-World-7B-v2.1-20240507-ctx4096.pth', strategy='cuda bf16')
-#model ='rwkv'
 pipeline = PIPELINE(model, "rwkv_vocab_v20230424")
 
 
@@ -33,6 +30,9 @@ msg3 = ["Q: 介绍一下openai这个公司，以及他的产品\n\nA:"]
 token, mask = pipeline.encode_bsz(msg2)
 print('tokens:',token)
 print('mask:', mask)
+
+prefill,states = model.forward(token, state=None, mask=mask)
+
 answer, state = pipeline.gen_bsz(msg2, token_count=500, args=args)
 
 print(answer, len(state))
